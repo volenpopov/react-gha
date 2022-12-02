f219909cb89403b6c64644ddee6632644b8cd5a8
Volen Popov <volenpopov@gmail.com>
2022-12-02 15:57:53 +0200

    add dummy text

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload. BRAVO ugh
+          Edit <code>src/App.tsx</code> and save to reload. WELLDONE!!!!
         </p>
         <a
           className="App-link"
